1803333b3924db1b7c459b93bce083179f570c18
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-13 14:13:08 +0700

    Update and rename textpro.js to media.js

---
+++ src/media.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch')
+const base = 'https://sosmed-api-zhirrr.vercel.app'
+
+module.exports.igstalk = function igstalk(query) {
+	return new Promise((resolve, reject) => {
+		fetch(base+'/api/ig/stalk?user=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.igdown = function igdown(query) {
+	return new Promise((resolve, reject) => {
+		fetch(base+'/api/ig?url=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.tiktokdown = function tiktokdown(query) {
+	return new Promise((resolve, reject) => {
+		fetch(base+'/api/tiktok?url=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
