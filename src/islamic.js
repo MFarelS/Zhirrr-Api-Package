6b41073f055c820eb1ed3667d9eae8f0ea6dd956
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-13 14:04:35 +0700

    Update and rename media.js to islamic.js

---
+++ src/islamic.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch')
+const asmaul = 'https://raw.githubusercontent.com/Zhirrr/islamic-rest-api-indonesian/main/data/dataAsmaulHusna.json'
+const tahlilan = 'https://raw.githubusercontent.com/Zhirrr/islamic-rest-api-indonesian/main/data/dataTahlil.json'
+const kursi = 'https://raw.githubusercontent.com/Zhirrr/islamic-rest-api-indonesian/main/data/dataAyatKursi.json'
+const nabi = 'https://kisahnabi-api-zhirrr.vercel.app'
+const wiridd = 'https://raw.githubusercontent.com/Zhirrr/islamic-rest-api-indonesian/main/data/dataWirid.json'
+
+module.exports.asmaulhusna = function asmaulhusna() {
+	return new Promise((resolve, reject) => {
+		fetch(asmaul)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.tahlil = function tahlil() {
+	return new Promise((resolve, reject) => {
+		fetch(tahlilan)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.ayatkursi = function ayatkursi() {
+	return new Promise((resolve, reject) => {
+		fetch(kursi)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.kisahnabi = function kisahnabi(query) {
+	return new Promise((resolve, reject) => {
+		fetch(nabi+'/api/searchnabi?q=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.wirid = function wirid() {
+	return new Promise((resolve, reject) => {
+		fetch(wiridd)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
