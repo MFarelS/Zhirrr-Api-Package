51db0d202688a9fee1794830f20a534cc4740275
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-13 14:16:52 +0700

    Update media.js

--- src/media.js
+++ src/media.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
 const base = 'https://sosmed-api-zhirrr.vercel.app'
+const facebook = 'https://fb-api-zhirrr.vercel.app'
 
 module.exports.igstalk = function igstalk(query) {
 	return new Promise((resolve, reject) => {
@@ -22,6 +23,15 @@ module.exports.igdown = function igdown(query) {
 module.exports.tiktokdown = function tiktokdown(query) {
 	return new Promise((resolve, reject) => {
 		fetch(base+'/api/tiktok?url=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.fbdown = function fbdown(query) {
+	return new Promise((resolve, reject) => {
+		fetch(facebook+'/?url=' + encodeURIComponent(query))
 		.then(res => res.json())
 		.then(resolve)
 		.catch(reject)
