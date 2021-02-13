ee3a3f85438b72b24139ea1b7eeb4ee74ca775d5
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-13 14:17:40 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 exports.sosmed = require('./media');
-exports.textpro = require('./textpro');
+exports.muslim = require('./islamic');
 exports.edu = require('./education');
 exports.flaming = require('./flaming')
